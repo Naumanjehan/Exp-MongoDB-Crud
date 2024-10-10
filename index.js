08aadef612cb434d5c38f5dd8e70c586bf0dc0af
Naumanjehan <haristrader321@gmail.com>
2024-10-10 15:51:57 +0500

    user created

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose.connect(mongoUrl, {
     console.log('mongodb connection error', err.message)
 })
 
-const userSchema = new mongoose.connect({
+const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   age: Number
@@ -26,11 +26,29 @@ const userSchema = new mongoose.connect({
 const User = mongoose.model("User", userSchema) //there will be singular name
 
 //FETCHING  ALL USER
-app.get('/user', async(req, res) =>{
+app.get('/users', async(req, res) =>{
   try {
     const users = await  User.find();
-    res.json(users)
+    res.status(200).json({message: "fetching all users", data: users})
   } catch (error) {
     console.log(error)
+    res.status(500).json({message: "error in fetching users"})
   }
 })
+
+//CREATE USERS
+app.post('/users', async(req, res) =>{
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age
+    })
+    try {
+       const allUser = await newUser.save()
+       res.status(200).json({message: "creating new user successful", data: allUser})
+    } catch (error) {
+        console.log(error)
+         res.status(500).json({message: "error in creating users"})
+    }
+})
+
